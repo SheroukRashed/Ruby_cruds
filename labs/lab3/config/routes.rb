1d41d637ed4533669b7a96c80a05ab26a8fc6597
sheroukRashed <eng_sheroukrashed@yahoo.com>
2019-04-20 17:57:21 +0200

    login signup signout

--- labs/lab3/config/routes.rb
+++ labs/lab3/config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-
+  devise_for :users
+  
   root 'welcome#index'
 end
